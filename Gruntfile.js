189fb1d85201c5cf6e957a2efd9a5a0d41af734d
Dave Wallace <dave.wallace@kineo.co.nz>
2015-12-07 17:52:53 +1300

    Removed call to clean task that is not ready to ship yet.

--- Gruntfile.js
+++ Gruntfile.js
@@ -206,7 +206,7 @@ module.exports = function(grunt) {
 
     grunt.task.run(
       'copy:renameStubFilesAndContents',
-      'clean:removeStubFiles'
+      //'clean:removeStubFiles'
     );
 
   });
